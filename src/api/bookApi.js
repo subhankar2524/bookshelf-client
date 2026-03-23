95882c916d3803581fb6a0ca50f5a51fa01c92d4
Subhankar Ghosh <subhankarg2524@gmail.com>
2026-03-23 05:30:46 +0530

    ...

--- src/api/bookApi.js
+++ src/api/bookApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: process.env.REACT_APP_API_BASE_URL,
 });
 
 export const searchBooksApi = (query) => API.get(`/books?q=${query}`);
--- src/api/userActionApi.js
+++ src/api/userActionApi.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken } from "../utils/storage";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: process.env.REACT_APP_API_BASE_URL,
 });
 
 API.interceptors.request.use((config) => {
